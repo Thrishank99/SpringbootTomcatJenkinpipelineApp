c1008f1ebf056b965d06232f6cbf3ce72e90553c
Thrishank99 <thrishank966@gmail.com>
2024-10-21 21:22:52 +0530

    first commit

---
+++ src/main/java/com/org/java/SpringbootTomcatJenikinPipelineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTomcatJenikinPipelineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTomcatJenikinPipelineProjectApplication.class, args);
+	}
+
+}
